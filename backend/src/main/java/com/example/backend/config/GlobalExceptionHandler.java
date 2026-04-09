c6b985b5eba6556ebf26eacda7e952aa477281ef
@rayuthek430 <rayuthek430@gmail.com>
2026-04-09 21:10:34 +0700

    pull

--- backend/src/main/java/com/example/backend/config/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/backend/config/GlobalExceptionHandler.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 @RestController
 public class GlobalExceptionHandler {
